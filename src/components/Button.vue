018a18296973b2a995f200167633a07116da7e4a
Stuart Sewell <stuart.sewell@nurole.com>
2021-01-26 12:05:20 +0000

    Initial

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<button @click="increment">{{ text }}</button>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Button extends Vue {
+	count = 0;
+
+	get times() {
+		return this.count === 1 ? "time" : "times";
+	}
+
+	get text() {
+		return `I have been clicked ${this.count} ${this.times}`;
+	}
+
+	increment() {
+		this.count++;
+	}
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Button from "./components/Button.vue";
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export { Button };
